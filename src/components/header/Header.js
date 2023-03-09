2aae3abe5883d20fc5dfdb503f110da5c2aa0deb
Sukenpoud <axilais.romain@gmail.com>
2023-03-09 15:22:28 +0100

    début couleurs header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -30,7 +30,7 @@ function Header() {
               <Button color="pink">Nouveau trajet</Button>
             </Menu.Target>
 
-            <Menu.Dropdown>
+            <Menu.Dropdown className='nav-dropdown-trajet'>
               <Menu.Item icon={<BiArrowToTop size={14} />}>Publier un trajet</Menu.Item>
               <Menu.Item icon={<BiSearch size={14} />}>Rechercher un trajet</Menu.Item>
             </Menu.Dropdown>
@@ -40,7 +40,7 @@ function Header() {
                 <Avatar src={avatar} />
             </Menu.Target>
 
-            <Menu.Dropdown>
+            <Menu.Dropdown className='nav-dropdown-profil'>
               <Link to='/connexion'><Menu.Item icon={<BiPlanet size={14} />}>Connexion</Menu.Item></Link>
               <Link to='/inscription'><Menu.Item icon={<BiPlanet size={14} />}>Inscription</Menu.Item></Link>
               <Menu.Item icon={<BiPlanet size={14} />}>Profil</Menu.Item>
