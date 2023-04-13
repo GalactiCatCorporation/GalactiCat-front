904fdf2036444c023ad1d1547c66693e03a5deab
Nicolas Baca <nicobaca17@gmail.com>
2023-04-13 11:19:32 +0200

    error management signin/signup

--- src/views/auth/Signin.js
+++ src/views/auth/Signin.js
@@ -2,17 +2,46 @@ import React from 'react'
 import { TextInput, PasswordInput, Button } from '@mantine/core';
 import '../../index.scss';
 import './Auth.scss';
+import axios from 'axios';
+import { notifications } from '@mantine/notifications';
+import { useNavigate } from "react-router-dom";
 
 function Signin() {
+  const [pseudo, setPseudo] = React.useState();
+  const [password, setPassword] = React.useState();
+
+  const navigate = useNavigate();
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    axios
+      .post("http://localhost:8888/api/v1/auth/authenticate", { 'email': pseudo, password })
+      .then(response => {
+        localStorage.setItem('token', response.data.token);
+        navigate("/");
+      })
+      .catch(error => {
+        notifications.show({
+          withCloseButton: true,
+          autoClose: 5000,
+          title: "C'est à nouveau le gui",
+          message: 'Pseudo ou mot de passe incorrect !',
+          color: 'red',
+        });
+      })
+  }
+
   return (
     <div id='Auth'>
       <h1>Se connecter</h1>
       <p>Vous n'avez pas encore de compte ? <a href="/inscription">Créer un compte</a></p>
       <div className='content'>
-        <form>
+        <form method="post" onSubmit={handleSubmit}>
           <TextInput
             placeholder="Pseudonyme"
             label="Pseudonyme"
+            onChange={e => setPseudo(e.target.value)}
             withAsterisk
           />
           <PasswordInput
@@ -20,6 +49,7 @@ function Signin() {
             placeholder="Mot de passe"
             label="Mot de passe"
             className='password'
+            onChange={e => setPassword(e.target.value)}
             withAsterisk
           />
           <div className='submit-button'>
--- src/views/auth/Signup.js
+++ src/views/auth/Signup.js
@@ -1,34 +1,87 @@
 import React from 'react'
-import { Input, PasswordInput, Button } from '@mantine/core';
+import { TextInput, PasswordInput, Button } from '@mantine/core';
 import '../../index.scss';
 import './Auth.scss';
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+import { notifications } from '@mantine/notifications';
 
 function Signup() {
+  const [pseudo, setPseudo] = React.useState();
+  const [password, setPassword] = React.useState();
+  const [confirmPassword, setConfirmPassword] = React.useState();
+
+  const navigate = useNavigate();
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    const isValidPassword = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(password);
+
+    if (isValidPassword) {
+      if (password !== confirmPassword) {
+        notifications.show({
+          withCloseButton: true,
+          autoClose: 5000,
+          title: "Eh gogole !",
+          message: 'Les mots de passe ne correspondent pas !',
+          color: 'red',
+        });
+      } else {
+        axios
+          .post("http://localhost:8888/api/v1/auth/register", { 'email': pseudo, password })
+          .then(response => {
+            localStorage.setItem('token', response.data.token);
+            navigate("/");
+          })
+          .catch(error => {
+            notifications.show({
+              withCloseButton: true,
+              autoClose: 5000,
+              title: "C'est à nouveau le gui",
+              message: 'Pseudo ou mot de passe incorrect !',
+              color: 'red',
+            });
+          })
+      }
+    } else {
+      notifications.show({
+        withCloseButton: true,
+        autoClose: 4000,
+        title: "Yo c'est le Gui qui parle",
+        message: 'Le mot de passe doit contenir une lettre, un chiffre et faire minimum 8 caractères !',
+        color: 'red',
+      });
+    }
+
+  }
   return (
     <div id="Auth">
       <h1>S'inscrire</h1>
       <p>Vous avez déjà un compte ? <a href="/connexion">Se connecter</a></p>
       <div className='content'>
-        <form>
-          <Input.Wrapper
+        <form method="post" onSubmit={handleSubmit}>
+          <TextInput
+            placeholder="Pseudonyme"
             label="Pseudonyme"
-            required
-          >
-            <Input
-              placeholder="Pseudonyme"
-            />
-          </Input.Wrapper>
+            onChange={e => setPseudo(e.target.value)}
+            withAsterisk
+            maxLength="15"
+          />
           <PasswordInput
             required
             placeholder="Mot de passe"
             label="Mot de passe"
             className='password'
+            onChange={e => setPassword(e.target.value)}
             withAsterisk
           />
+          <span className='info-password'>Doit contenir une lettre, une chiffre et doit faire minimum 8 caractères</span>
           <PasswordInput
             required
             placeholder="Confirmation mot de passe"
             label="Confirmation mot de passe"
+            onChange={e => setConfirmPassword(e.target.value)}
             className='password'
             withAsterisk
           />
